a36fd43e98075f68630967fd01bb36bb2a968836
paul <you@yourdomain.example.com>
2023-02-15 20:15:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+function xdecimal() {
+  decimal = document.getElementById("decimal");
+  hexadecimal = document.getElementById("hexadecimal");
+  binaire = document.getElementById("binaire");
+
+  hexadecimal.value = (decimal.value - 0).toString(16);
+  binaire.value = (decimal.value - 0).toString(2);
+}
+
+function xhexadecimal() {
+  decimal = document.getElementById("decimal");
+  hexadecimal = document.getElementById("hexadecimal");
+  binaire = document.getElementById("binaire");
+
+  decimal.value = parseInt(xhexadecimal.value, 16);
+  binaire.value = parseInt(xhexadecimal.value, 16).toString(2);
+}
+
+function xbinaire() {
+  decimal = document.getElementById("decimal");
+  hexadecimal = document.getElementById("hexadecimal");
+  binaire = document.getElementById("binaire");
+
+  decimal.value = parseInt(xbinaire.value, 2);
+  hexadecimal.value = parseInt(xbinaire.value, 2).toString(16);
+}
+// Cette partie du code permet de faire des  calculer dans les bases
+function setBase() {
+  base1 = document.getElementById("base1");
+  base2 = document.getElementById("base2");
+  base3 = document.getElementById("base3");
+  if (base1 != null && Number(base1.value) === 10) {
+    base2.value = "16";
+    base3.value = "2";
+  } else if (base2 != null && Number(base2.value) === 16) {
+    base1.value = "10";
+    base3.value = "2";
+  } else if (base3 != null && Number(base3.value) === 2) {
+    base1.value = "10";
+    base2.value = "16";
+  }
+}
+
+//Tests
+
+// check if string is hexadecimal(check si le nombre est hexadecimal)
+function isHex(h) {
+  let a = parseInt(h, 16);
+  return a.toString(16) === h.toLowerCase();
+}
+
+// check if string is decimal(check si le nombre est decimal)
+isdigit();
+
+// check of string in binary(check si le nombre est binaire)
+function checkString(str) {
+  let isBinary = false;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "0" || str[i] == "1") {
+      isBinary = true;
+    } else {
+      isBinary = false;
+    }
+  }
+}
